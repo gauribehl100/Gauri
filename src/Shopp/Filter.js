110bc8af1ae837df175162e3c4972540cc00701c
gauribehl100 <140520216+gauribehl100@users.noreply.github.com>
2024-07-27 12:31:01 +0530

    Filter.js

--- src/Shopp/Filter.js
+++ src/Shopp/Filter.js
@@ -2,17 +2,17 @@ import React from 'react'
 
 const Filter = (props) => {
   let filterData = props.filterData;
-  // let setCategory = props.setCategory;
-  /*function filterHandler (title) {
+  let setCategory = props.setCategory;
+  function filterHandler (title) {
     setCategory(title);
-    } */
+    } 
   return (
     <div className='filterr'>
         {filterData.map((data) => {
             return(<button
              className='filterdata'
              key={data.id}
-            //  onClick={filterHandler(data.title)}
+            onClick={filterHandler(data.title)}
              >{data.title}
             </button>)
         })}
